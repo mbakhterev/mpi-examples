248237c121e596181b38b3a9da4836bf6df2c219
M.Bakhterev <mob@k.imm.uran.ru>
2021-04-26 09:18:52 +0500

    Примеры reduce

--- python/reduce.py
+++ python/reduce.py
@@ -11,14 +11,62 @@ def separate():
         print("", flush = True)
     W.barrier()
 
-data = np.zeros(10, dtype = 'i')
-data[0] = rank
+# Vectors
 
-r = np.zeros(1, dtype = 'i')
+if rank == 0:
+    print("Vectors", flush = True)
+
+separate()
+
+data = rank * np.arange(10, dtype = 'i')
+
+print("{}: data: {}".format(rank, data), flush = True)
+
+separate()
+
+r = np.empty(10, dtype = 'i')
 
 W.Reduce(data, r, root = 0, op = MPI.SUM)
 
 if rank == 0:
-    print("{}: Reduce {}".format(rank, r), flush = True)
+    print("{}: result: {}".format(rank, r), flush = True)
 
+separate()
+
+# Matrix 
+
+if rank == 0:
+    print("Matrices", flush = True)
+
+separate()
+
+data = rank * np.arange(9, dtype = 'i').reshape(3, 3)
+
+print("{}: data: {}".format(rank, data), flush = True)
+
+separate()
+
+r = np.empty(9, dtype = 'i').reshape(3, 3)
+
+W.Reduce(data, r, root = 2, op = MPI.SUM)
+
+if rank == 2:
+    print("{}: result {}".format(rank, r), flush = True)
+
+separate()
+
+# Allreduce and order negotiation 
+
+if rank == 0:
+    print("Allreduce", flush = True)
+
+separate()
+
+W.Allreduce(data, r, op = MPI.MAX)
+
+for i in range(0, W.size):
+    W.barrier()
+    if i == rank:
+        print("{}: result: {}".format(rank, r), flush = True)
+        
 separate()
